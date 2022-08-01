50fd356347c2c96e79bc3774649260e8175c76ef
Frangu Madalin <polarfrann@gmail.com>
2022-08-01 21:22:57 +0300

    changed field relation

--- ngBackend/comments/models.py
+++ ngBackend/comments/models.py
@@ -5,7 +5,7 @@ from announcements.models import Announcement
 # Create your models here.
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
+    user = models.ForeignKey(User, to_field='username', on_delete=models.CASCADE, related_name="comment")
     announcement = models.ForeignKey(Announcement, related_name="announcements_comment", on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     content = models.TextField()
