4c616a80e193dcc36e4c2c2c7667562e6468c673
Frangu Madalin <polarfrann@gmail.com>
2022-08-01 21:23:18 +0300

    added try/catch blocks

--- ngBackend/comments/views.py
+++ ngBackend/comments/views.py
@@ -9,23 +9,21 @@ from announcements.models import Announcement
 from rest_framework.response import Response
 from rest_framework_extensions.mixins import NestedViewSetMixin
 from comments.serializers import CommentSerializer
-from rest_framework.exceptions import PermissionDenied
-
+from django.contrib.auth.models import User
 class GetCommentsViewset(NestedViewSetMixin, viewsets.ViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
     def create(self, request, *args, **kwargs):
-        if self.request.user.is_staff:
-            try:
-                announcement_id = self.kwargs.get("parent_lookup_announcements")
-                content = request.data['content']
-                Comment.objects.create(announcement_id=announcement_id, user=self.request.user, content=content)
-                return Response({"success": "data posted"})
-            except:
-                return Response({"error": "Error when trying to create comment"})
-        else:
-            raise PermissionDenied({"error": "You are not authorized to post to this endpoint."})
+        try:
+            announcement_id = self.kwargs.get("parent_lookup_announcements")
+            content = request.data['content']
+            user = request.data['user']
+            user = User.objects.get(username=user)
+            Comment.objects.create(announcement_id=announcement_id, user=user, content=content)
+            return Response({"success": "data posted"})
+        except:
+            return Response({"error": "Error when trying to create comment"})
 
     def list(self, request, *args, **kwargs):
         queryset = Comment.objects.filter(announcement_id=self.kwargs.get("parent_lookup_announcements"))
@@ -38,3 +36,16 @@ class GetCommentsViewset(NestedViewSetMixin, viewsets.ViewSet):
         comment = get_object_or_404(queryset, pk=pk)
         serializer = CommentSerializer(comment)
         return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            queryset = Comment.objects.filter(announcement_id=self.kwargs.get("parent_lookup_announcements"))
+            pk = self.kwargs.get("pk")
+            comment = get_object_or_404(queryset, pk=pk)
+            if (comment.user != request.user):
+                return Response({"error": "You are not allowed to delete this comment"})
+
+            comment.delete()
+            return Response({"success": "Comment deleted"})
+        except:
+            return Response({"error": "Error when trying to delete comment"})
