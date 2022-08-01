59282f076706898b77cba130549ce61e223e9892
Frangu Madalin <polarfrann@gmail.com>
2022-08-01 21:26:33 +0300

    added exclusion

--- ngBackend/user_profiles/UserProfileSerializer.py
+++ ngBackend/user_profiles/UserProfileSerializer.py
@@ -4,4 +4,4 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = "__all__"
+        exclude = ('user', )
